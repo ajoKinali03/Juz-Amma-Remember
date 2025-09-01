b508f15ababa2708996d426f80b96707fb1a2411
ajoKinali03 <functionpublic10@gmail.com>
2025-09-02 05:03:34 +0700

    progres membuat sistem pertanyaan

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const loadData = async function (file) {
 
 let globalValue = document.getElementById("global-value");
 
+const gameType = ["Apa Ayat Sesudahnya?", "Apa Ayat Sebelumnya?", "Tebak Nomor Ayat ini?", "Tebak Surat Dari Ayat ini?", "Apa Terjemahannya?"];
+
 //menampilkan data surah
 loadData("./surah.json").then((surah) => {
   let btnSurah = document.getElementById("btn-surah");
@@ -17,18 +19,22 @@ loadData("./surah.json").then((surah) => {
 
   for (item of surah) {
     let option = document.createElement("option");
-    option.value = item.title; // value
-    option.textContent = item.title; // teks yang terlihat
+    option.value = item.title;
+    option.textContent = item.title;
     selectSurah.appendChild(option);
   }
 
   btnSurah.addEventListener("click", () => {
     selectSurah.style.display = "block";
   });
-  
+
   selectSurah.addEventListener("click", () => {
-    globalValue.innerHTML = selectSurah.value;
-  })
+    for (item of surah) {
+      if (selectSurah.value === item.title) {
+        globalValue.innerHTML = JSON.stringify(item);
+      }
+    }
+  });
 
   document.addEventListener("click", (el) => {
     if (el.target.innerText !== "Surah" && el.target.localName !== "option") {
@@ -37,4 +43,30 @@ loadData("./surah.json").then((surah) => {
   });
 });
 
+let ques = document.getElementById("question");
+let startBtn = document.getElementById("start");
 
+let fileNameSurah = Array.from({ length: 114 - 78 + 1 }, (_, i) => i + 78);
+
+function randomData(countData) {
+  if (countData.length === 0) {
+    return null;
+  }
+  const randomIndex = Math.floor(Math.random() * countData.length);
+  const value = countData[randomIndex];
+  countData.splice(randomIndex, 1);
+  return value;
+}
+
+if (globalValue.innerHTML === "all") {
+  startBtn.addEventListener("click", () => {
+    loadData(`./surah/surah_${randomData(fileNameSurah)}.json`).then((res) => {
+      let nameSurah = Array.from({ length: res.count - 1 + 1 }, (_, i) => i + 1);
+      console.log(randomData(nameSurah));
+    });
+  });
+} else {
+  startBtn.addEventListener("click", () => {
+    ques.innerHTML = globalValue.innerHTML;
+  });
+}
