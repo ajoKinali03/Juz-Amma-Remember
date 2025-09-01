cb000687e018ee4c11534327d57b9bf2711dc083
ajoKinali03 <functionpublic10@gmail.com>
2025-09-02 03:03:12 +0700

    mengatur data yang tampil pada tombol surah

--- main.js
+++ main.js
@@ -8,10 +8,33 @@ const loadData = async function (file) {
   }
 };
 
+let globalValue = document.getElementById("global-value");
+
+//menampilkan data surah
 loadData("./surah.json").then((surah) => {
-  for(item of surah){
-    if(item.place === "Mecca"){
-      console.log(item)
-    };
-  };
+  let btnSurah = document.getElementById("btn-surah");
+  let selectSurah = document.getElementById("select-surah");
+
+  for (item of surah) {
+    let option = document.createElement("option");
+    option.value = item.title; // value
+    option.textContent = item.title; // teks yang terlihat
+    selectSurah.appendChild(option);
+  }
+
+  btnSurah.addEventListener("click", () => {
+    selectSurah.style.display = "block";
+  });
+  
+  selectSurah.addEventListener("click", () => {
+    globalValue.innerHTML = selectSurah.value;
+  })
+
+  document.addEventListener("click", (el) => {
+    if (el.target.innerText !== "Surah" && el.target.localName !== "option") {
+      selectSurah.style.display = "none";
+    }
+  });
 });
+
+
