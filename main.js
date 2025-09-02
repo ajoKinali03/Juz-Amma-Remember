16edc728500564213006f9bcbfae1674e92a2643
ajoKinali03 <functionpublic10@gmail.com>
2025-09-03 02:39:01 +0700

    progres membuat opsi jawaban

--- main.js
+++ main.js
@@ -8,9 +8,23 @@ const loadData = async function (file) {
   }
 };
 
+// nilai global
 let globalValue = document.getElementById("global-value");
 
-const gameType = [
+// mengacak angka
+function randomData(max, min) {
+  let countData = Array.from({ length: max - min + 1 }, (_, i) => i + min);
+  if (countData.length === 0) {
+    return null;
+  }
+  const randomIndex = Math.floor(Math.random() * countData.length);
+  const value = countData[randomIndex];
+  countData.splice(randomIndex, 1);
+  return value;
+}
+
+// array pertanyaan
+let gameType = [
   "Apa Ayat Sesudahnya?",
   "Apa Ayat Sebelumnya?",
   "Tebak Nomor Ayat ini?",
@@ -53,37 +67,91 @@ let ques = document.getElementById("question");
 let quest = document.getElementById("quest");
 let startBtn = document.getElementById("start");
 
-let fileNameSurah = Array.from({ length: 114 - 78 + 1 }, (_, i) => i + 78);
-
-function randomData(countData) {
-  if (countData.length === 0) {
-    return null;
-  }
-  const randomIndex = Math.floor(Math.random() * countData.length);
-  const value = countData[randomIndex];
-  countData.splice(randomIndex, 1);
-  return value;
-}
 startBtn.addEventListener("click", () => {
   if (globalValue.innerHTML === "all") {
-    loadData(`./surah/surah_${randomData(fileNameSurah)}.json`).then((res) => {
-      let nameSurah = Array.from(
-        { length: res.count - 1 + 1 },
-        (_, i) => i + 1
-      );
-      console.log(res.name);
-      console.log("all");
-      ques.innerHTML = eval("res.verse.verse_" + randomData(nameSurah));
+    loadData(`./surah/surah_${randomData(114, 78)}.json`).then((res) => {
+      let verseId = randomData(res.count, 1);
+      if (res.count === verseId) {
+        gameType = gameType.filter((e) => e != "Apa Ayat Sesudahnya?");
+        let curentGameType = gameType[randomData(gameType.length - 1, 0)];
+        checkAnswer(verseId, res, curentGameType);
+        quest.innerHTML = curentGameType;
+        ques.innerHTML = eval("res.verse.verse_" + verseId);
+      } else {
+        let curentGameType = gameType[randomData(gameType.length - 1, 0)];
+        checkAnswer(verseId, res, curentGameType);
+        quest.innerHTML = curentGameType;
+        ques.innerHTML = eval("res.verse.verse_" + verseId);
+      }
     });
   } else {
     let dataSurah = JSON.parse(globalValue.innerHTML);
-    let idxSurah = parseInt(dataSurah.index)
+    let idxSurah = parseInt(dataSurah.index);
     loadData(`./surah/surah_${idxSurah}.json`).then((res) => {
-      let nameSurah = Array.from(
-        { length: res.count - 1 + 1 },
-        (_, i) => i + 1
-      );
-      ques.innerHTML = eval("res.verse.verse_" + randomData(nameSurah));
+      gameType = gameType.filter((e) => e != "Tebak Surat Dari Ayat ini?");
+      quest.innerHTML = gameType[randomData(gameType.length - 1, 0)];
+      ques.innerHTML = eval("res.verse.verse_" + randomData(res.count, 1));
     });
   }
 });
+
+// fungsi untuk menampilkan jawaban secara random
+function checkAnswer(curent, data, curentQuest) {
+  let key = randomData(4, 1);
+  for (let i = 1; i <= 4; i++) {
+    if (curentQuest === "Apa Ayat Sesudahnya?") {
+      if (i == key) {
+        document.getElementById(key).innerHTML = eval(
+          "data.verse.verse_" + (curent + 1)
+        );
+      } else {
+        document.getElementById(i).innerHTML = eval(
+          "data.verse.verse_" + randomData(data.count, 1)
+        );
+      }
+    } else if (curentQuest === "Apa Ayat Sebelumnya?") {
+      if (i == key) {
+        document.getElementById(key).innerHTML = eval(
+          "data.verse.verse_" + (curent - 1)
+        );
+      } else {
+        document.getElementById(i).innerHTML = eval(
+          "data.verse.verse_" + randomData(data.count, 1)
+        );
+      }
+    } else if (curentQuest === "Tebak Nomor Ayat ini?") {
+      if (i == key) {
+        document.getElementById(key).innerHTML = "Ayat ke-" + curent;
+      } else {
+        while (true) {
+          let ayatIdx = randomData(data.count, 1);
+          if (ayatIdx != curent) {
+            document.getElementById(i).innerHTML = "Ayat ke-" + ayatIdx;
+          } else {
+            break;
+          }
+        }
+      }
+    } else if (curentQuest === "Tebak Surat Dari Ayat ini?") {
+      if (i == key) {
+        document.getElementById(key).innerHTML = data.name;
+      } else {
+        loadData("./surah").then((res) => {
+          while (true) {
+            // mengatur keluar nama surah
+            let surahIdx = randomData(114, 78);
+            if (true) {
+              document.getElementById(i).innerHTML = "Surah ke-" + surahIdx;
+            } else {
+              break;
+            }
+          }
+        });
+      }
+    } else if (curentQuest === "Apa Terjemahannya?") {
+      document.getElementById(i).innerHTML = "";
+    } else {
+      console.error("pertanyaan tidak ditemukan");
+    }
+  }
+}
